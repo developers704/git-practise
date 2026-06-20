bcd90f017817efadf38d46d17c8717ccacf943d0
Umair <umairjam.arrakconsulting@gmail.com>
2026-06-21 02:06:38 +0500

    add divide function

--- app.py
+++ app.py
@@ -5,8 +5,13 @@ def main():
     print("Hello Umair")
     print("2 + 3 =", add(2, 3))
     print("4 * 5 =", multiply(4, 5))
+    print("10 / 2 =", divide(10, 2))
 
 def multiply(a, b):
     return a * b
 if __name__ == "__main__":
     main()
+
+def divide(a, b):
+    return a / b
+
