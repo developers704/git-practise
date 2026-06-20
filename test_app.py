0a9b84fc7e1d1325c6c7c40b14ca015b55f04ceb
Umair <umairjam.arrakconsulting@gmail.com>
2026-06-21 03:36:35 +0500

    add automated tests

---
+++ test_app.py
@@ -0,0 +1,13 @@
+from app import add, multiply, divide, subtract
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_multiply():
+    assert multiply(4, 5) == 20
+
+def test_divide():
+    assert divide(10, 2) == 5
+
+def test_subtract():
+    assert subtract(10, 3) == 7
