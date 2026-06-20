4b2def7fe3e5ab1e04a6eff6f96e30c3977886fe
Umair <umairjam.arrakconsulting@gmail.com>
2026-06-21 01:56:10 +0500

    add multiply function

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ def add(a, b):
 def main():
     print("Hello Umair")
     print("2 + 3 =", add(2, 3))
+    print("4 * 5 =", multiply(4, 5))
 
+def multiply(a, b):
+    return a * b
 if __name__ == "__main__":
     main()
